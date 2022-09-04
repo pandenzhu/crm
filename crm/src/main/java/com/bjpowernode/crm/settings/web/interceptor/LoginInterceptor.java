0b65f141fd60cb27dd6888d5dfbf65c38c578175
pandeng_zhu <84564241@qq.com>
2022-09-04 09:57:13 +0800

    安全登录功能（登录拦截器） 测试完成

--- crm/src/main/java/com/bjpowernode/crm/settings/web/interceptor/LoginInterceptor.java
+++ crm/src/main/java/com/bjpowernode/crm/settings/web/interceptor/LoginInterceptor.java
@@ -1,17 +1,29 @@
 package com.bjpowernode.crm.settings.web.interceptor;
 
+import com.bjpowernode.crm.commons.contants.Contants;
+import com.bjpowernode.crm.settings.domain.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //如果用户没有登录成功，则跳转到登录成功的界面
-
-        return false;
+        //判断是否有session，如果没有则没有登录，则跳转到登录的界面
+        /*
+        request.getContextPath():动态获取项目名称
+        response.sendRedirect():重定向时跳转到首页
+         */
+        HttpSession session=request.getSession();
+        User user = (User) session.getAttribute(Contants.SESSION_USER);
+        if (user==null){
+            response.sendRedirect(request.getContextPath());//重定向时，url需要加项目名称
+            return false;
+        }
+        return true;
     }
 
     @Override
