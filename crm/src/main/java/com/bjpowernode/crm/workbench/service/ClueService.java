e4b9463f4ec3916a99d50671e05c5074f045bd83
pandeng_zhu <84564241@qq.com>
2022-10-15 11:43:36 +0800

    1.关联市场活动，排除已关联的市场活动 2.根据ids查询市场活动明细

--- crm/src/main/java/com/bjpowernode/crm/workbench/service/ClueService.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/service/ClueService.java
@@ -15,16 +15,17 @@ public interface ClueService {
     Clue queryClueForDetailById(String id);
 
     //根据条件分页查询线索的列表
-    List<Clue> queryClueByConditionForPage(Map<String,Object>map);
+    List<Clue> queryClueByConditionForPage(Map<String, Object> map);
 
     //根据条件查询线索数量
-    int queryCountClueByCondition(Map<String,Object>map);
+    int queryCountClueByCondition(Map<String, Object> map);
 
     //根据id查询线索信息
     Clue queryClueById(String id);
 
     //更新线索
     int saveEditClue(Clue clue);
-//删除线索
+
+    //删除线索
     int deleteClueByIds(String[] id);
 }
